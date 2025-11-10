7dc12f7af9a455d5e3ed3d626540e070d6b962c9
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-10 16:44:25 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,15 @@
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
+variable "client_id" {
+  description = "Azure AD App Registration Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure AD App Registration Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure AD Tenant ID"
+  type        = string
+}
