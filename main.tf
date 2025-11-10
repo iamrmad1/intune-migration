7e76537b82528bd56c40fe8c4891760172d3979c
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-10 16:50:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,10 @@
-resource "microsoft365wp_device_configuration_policy_windows10" "defender_policy" {
-  display_name = "Windows Defender Antivirus Policy"
-  description  = "Simple configuration profile for Windows 10"
+resource "microsoft365wp_device_compliance_policy_windows10" "example" {
+  display_name = "Windows 10 Compliance Policy"
+  description  = "Ensure Defender and BitLocker are enabled"
 
-  settings_catalog {
-    settings = [
-      {
-        name       = "Allow AntiSpyware"
-        category   = "Microsoft Defender Antivirus"
-        value_type = "boolean"
-        value      = true
-      },
-      {
-        name       = "Turn on behavior monitoring"
-        category   = "Microsoft Defender Antivirus"
-        value_type = "boolean"
-        value      = true
-      }
-    ]
-  }
+  password_required = true
+  password_minimum_length = 8
+
+  defender_enabled = true
+  bitlocker_enabled = true
 }
